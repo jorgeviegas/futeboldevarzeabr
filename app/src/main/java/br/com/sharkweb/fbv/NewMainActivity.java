78547d828135f7d5c87698ff367cb7b4c3fae18f
Tiago <kleintiagomail@gmail.com>
2016-01-25 13:17:03 -0200

    - Iniciado tela de estatísticas do jogador.

--- app/src/main/java/br/com/sharkweb/fbv/NewMainActivity.java
+++ app/src/main/java/br/com/sharkweb/fbv/NewMainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
